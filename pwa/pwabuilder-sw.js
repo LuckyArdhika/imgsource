c81c638de99d3170bc8dd1a4a11ca8f8e7ea5bd1
Lucky Ardhika <49157897+LuckyArd110@users.noreply.github.com>
2021-09-25 21:20:10 +0700

    Update pwabuilder-sw.js

--- pwa/pwabuilder-sw.js
+++ pwa/pwabuilder-sw.js
@@ -1,43 +1,34 @@
-//This is the "Offline copy of pages" wervice worker
+const CACHE_NAME = 'v1.1.5';
 
-//Install stage sets up the index page (home page) in the cahche and opens a new cache
+var urlsToCache = [
+	'/'
+];
+
 self.addEventListener('install', function(event) {
-    var indexPage = new Request('index.html');
-    event.waitUntil(
-      fetch(indexPage).then(function(response) {
-        return caches.open('pwabuilder-offline').then(function(cache) {
-          console.log('[PWA Builder] Cached index page during Install'+ response.url);
-          return cache.put(indexPage, response);
-        });
-    }));
-  });
-  
-  //If any fetch fails, it will look for the request in the cache and serve it from there first
-  self.addEventListener('fetch', function(event) {
-    var updateCache = function(request){
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return fetch(request).then(function (response) {
-          console.log('[PWA Builder] add page to offline'+response.url)
-          return cache.put(request, response);
-        });
-      });
-    };
-  
-    event.waitUntil(updateCache(event.request));
-  
-    event.respondWith(
-      fetch(event.request).catch(function(error) {
-        console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-  
-        //Check to see if you have it in the cache
-        //Return response
-        //If not in the cache, then return error page
-        return caches.open('pwabuilder-offline').then(function (cache) {
-          return cache.match(event.request).then(function (matching) {
-            var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-            return report
-          });
-        });
-      })
-    );
-  })
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+			.then(function() {
+				self.skipWaiting();
+			})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			cache.match(event.request, {ignoreSearch: true});
+		})
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
