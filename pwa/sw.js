6669205faa11a88ce383989e5fe0356cca62f219
Lucky Ardhika <luckymahadiningrat@gmail.com>
2021-09-26 05:35:09 +0700

    updating sw.js

---
+++ pwa/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'v1.1.5';
+
+var urlsToCache = [
+	'/'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+			.then(function() {
+				self.skipWaiting();
+			})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			cache.match(event.request, {ignoreSearch: true});
+		})
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
